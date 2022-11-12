847694c01e9e3512e90c513f2a31c67c651a74ac
aditya vinayak <adityavinayak@adityas-MacBook-Air.local>
2022-11-12 19:03:03 +0530

    added oktaConfig in dashboard.js

--- src/config/oktaConfig.js
+++ src/config/oktaConfig.js
@@ -3,6 +3,6 @@ export const oktaConfig = {
     clientId: `0oa76x7zuelWnRbbu5d7`,
     issuer: "https://dev-02087076.okta.com/oauth2/default",
     redirectUri: `http://localhost:3000/dashboard`, 
-    scopes: ["openid", "profile", "email"],
+    scopes: ["openid", "email"],
     pkce: true,
 };
