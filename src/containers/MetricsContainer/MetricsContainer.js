3958c360d47b173323b9032168940bb74f4e4f2e
aditya vinayak <adityavinayak@adityas-MacBook-Air.local>
2022-12-04 17:46:33 +0530

    added piechart

--- src/containers/MetricsContainer/MetricsContainer.js
+++ src/containers/MetricsContainer/MetricsContainer.js
@@ -1,5 +1,69 @@
-const MetricsContainer = () => {
-    return <div>This is the metrics tab</div>
-}
+import { Chart as ChartJs, Tooltip, Title, ArcElement, Legend } from "chart.js";
+import { useState, useEffect } from "react";
+import { Doughnut } from "react-chartjs-2";
+import axiosPrivate from "../../api/axiosPrivate";
 
-export default MetricsContainer
+ChartJs.register(Tooltip, Title, ArcElement, Legend);
+
+const MetricsContainer = () => {
+  const [data, setData] = useState({
+    datasets: [
+      {
+        data: [10, 20, 30],
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 205, 86)",
+        ],
+      },
+    ],
+    labels: ["Red", "Yellow", "Blue"],
+  });
+  useEffect(() => {
+    axiosPrivate
+      .get("metrics")
+      .then((res) => {
+        const { data } = res || {};
+        console.log(res.data);
+        const { bugsCount, featuresCount, feedbacksCount } = data;
+        const label = ["Bug", "Feature", "Feedback"];
+        const chartData = [
+          bugsCount || 0,
+          featuresCount || 0,
+          feedbacksCount || 0,
+        ];
+        setData({
+          datasets: [
+            {
+              data: chartData,
+              backgroundColor: [
+                "rgb(255, 99, 132)",
+                "rgb(54, 162, 235)",
+                "rgb(255, 205, 86)",
+              ],
+            },
+          ],
+          labels: label,
+        });
+        return true;
+      })
+      .catch((err) => {
+        console.log(err);
+        return false;
+      });
+  }, []);
+  return (
+    <div
+      style={{
+        width: "30%",
+        height: "30%",
+        marginLeft: "40%",
+        marginTop: "15%",
+      }}
+    >
+      <Doughnut data={data} />
+    </div>
+  );
+};
+
+export default MetricsContainer;
