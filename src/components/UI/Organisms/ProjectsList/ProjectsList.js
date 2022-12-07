2e024b26dd05a8b6d556d3400b6fde6b948bbeff
Harish B <harishb@Harishs-MacBook-Air.local>
2022-12-07 18:20:05 +0530

    loading UI for projects done

--- src/components/UI/Organisms/ProjectsList/ProjectsList.js
+++ src/components/UI/Organisms/ProjectsList/ProjectsList.js
@@ -1,7 +1,19 @@
+import { FileSearchOutlined } from "@ant-design/icons";
 import ProjectCard from "../../Molecules/ProjectCard";
 import styles from "./ProjectsList.module.css";
 
 const ProjectsList = ({projectsList, updateProject}) => {
+
+    if(projectsList?.length === 0 ) {
+        return (
+            <div className = {styles.noTicketFound}>
+                <FileSearchOutlined className = {styles.icon} />
+                <span className = {styles.span1}>No projects found</span>
+                <span className = {styles.span2}>Try adjusting your search to find what you're looking for</span>
+            </div>
+        )
+    }
+
     return (
         <div className = {styles.projectsPreviewContainer}>
              <div className = {styles.title}>
